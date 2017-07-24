bc079888a59ef07d5a5bbdfeb59f57202cbba832
Sorin Valentin Vijoaica <wops@null.net>
2017-07-24 12:38:10 +0300

    Moved methods from the Extensions class to the Utility class.

--- Strings Editor/Utility.cs
+++ Strings Editor/Utility.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Strings_Editor
 {
@@ -56,6 +59,51 @@ namespace Strings_Editor
                     src.Length - (index + search.Length));
             }
             return dst;
+        }
+
+        /// <summary>
+        /// Returns the items of a drag&drop event
+        /// </summary>
+        public static string[] GetDragItems(this DragEventArgs e)
+        {
+            return (string[])e.Data.GetData(DataFormats.FileDrop, false);
+        }
+
+        #region InternalCheckIsWow64
+        static bool is64BitProcess = (IntPtr.Size == 8);
+        static bool is64BitOperatingSystem = is64BitProcess || InternalCheckIsWow64();
+
+        [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWow64Process(
+            [In] IntPtr hProcess,
+            [Out] out bool wow64Process
+        );
+        #endregion
+
+        /// <summary>
+        /// Determines whether or not the system is 64bit or not
+        /// </summary>
+        /// <returns></returns>
+        public static bool InternalCheckIsWow64()
+        {
+            if ((Environment.OSVersion.Version.Major == 5 && Environment.OSVersion.Version.Minor >= 1) ||
+                Environment.OSVersion.Version.Major >= 6)
+            {
+                using (Process p = Process.GetCurrentProcess())
+                {
+                    bool retVal;
+                    if (!IsWow64Process(p.Handle, out retVal))
+                    {
+                        return false;
+                    }
+                    return retVal;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
